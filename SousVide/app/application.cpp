edae10a40fd8786dcb0c94ebe9f12fb71f799c0d
alon24 <htpcfreak@gmail.com>
2015-08-28 12:03:51 +0300

    Fix reading temp to not wait, but use a timer, it seems stable now, and clock works ok too

--- SousVide/app/application.cpp
+++ SousVide/app/application.cpp
@@ -982,14 +982,94 @@ void checkTempTriggerRelay(float temp) {
 	}
 }
 
+void readTempData();
+Timer oneTempReadTimer;
+byte addr[8];
+byte data[12];
+byte type_s;
+
+void readAfterWait() {
+	byte present = 0;
+
+//	delay(1000);     // maybe 750ms is enough, maybe not
+	// we might do a ds.depower() here, but the reset will take care of it.
+
+	present = ds.reset();
+	ds.select(addr);
+	ds.write(0xBE);         // Read Scratchpad
+
+//	Serial.print("  Data = ");
+//	Serial.print(present, HEX);
+//	Serial.print(" ");
+	for ( int i = 0; i < 9; i++)
+	{
+		// we need 9 bytes
+		data[i] = ds.read();
+//		Serial.print(data[i], HEX);
+//		Serial.print(" ");
+	}
+//	Serial.print(" CRC=");
+//	Serial.print(OneWire::crc8(data, 8), HEX);
+//	Serial.println();
+
+	// Convert the data to actual temperature
+	// because the result is a 16 bit signed integer, it should
+	// be stored to an "int16_t" type, which is always 16 bits
+	// even when compiled on a 32 bit processor.
+	int16_t raw = (data[1] << 8) | data[0];
+	if (type_s)
+	{
+		raw = raw << 3; // 9 bit resolution default
+		if (data[7] == 0x10)
+		{
+		  // "count remain" gives full 12 bit resolution
+		  raw = (raw & 0xFFF0) + 12 - data[6];
+		}
+	} else {
+		byte cfg = (data[4] & 0x60);
+		// at lower res, the low bits are undefined, so let's zero them
+		if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+		else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+		else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+		//// default is 12 bit resolution, 750 ms conversion time
+	}
+
+	float celsius = (float)raw / 16.0;
+//	fahrenheit = celsius * 1.8 + 32.0;
+	debugf("  Temperature = %f Celsius ", celsius);
+	debugf("mem %d",system_get_free_heap_size());
+//	Serial.print("  Temperature = ");
+//	Serial.print(celsius);
+//	Serial.print(" Celsius, ");
+//	Serial.println(" Fahrenheit");
+//	Serial.println();
+
+	currentTemp = celsius;
+
+	//TODO:ilan
+	if(currentDisplayMode == Display_Info) {
+		infos->updateParamValue("temp", String(currentTemp, 2), display);
+//		InfoScreenPage* iScreen = infos->get(currentInfoScreenIndex);
+//		iScreen->updateParam("temp", String(currentTemp, 3), display);
+		display.display();
+	}
+
+	checkTempTriggerRelay(celsius);
+
+//	unsigned long end = millis();
+//	debugf("Temp took %lu",  (long)(end- start));
+
+	readTempTimer.initializeMs(1000, readTempData).startOnce();
+}
+
 void readTempData()
 {
 //	unsigned long start = millis();
 	byte i;
 	byte present = 0;
-	byte type_s;
-	byte data[12];
-	byte addr[8];
+//	byte type_s;
+//	byte data[12];
+//	byte addr[8];
 	float celsius, fahrenheit;
 
 	ds.reset_search();
@@ -999,6 +1079,7 @@ void readTempData()
 		Serial.println();
 		ds.reset_search();
 		delay(250);
+		readTempTimer.initializeMs(1000, readTempData).startOnce();
 		return;
 	}
 
@@ -1040,87 +1121,18 @@ void readTempData()
 	ds.select(addr);
 	ds.write(0x44, 1);        // start conversion, with parasite power on at the end
 
-	delay(1000);     // maybe 750ms is enough, maybe not
-	// we might do a ds.depower() here, but the reset will take care of it.
 
-	present = ds.reset();
-	ds.select(addr);
-	ds.write(0xBE);         // Read Scratchpad
-
-//	Serial.print("  Data = ");
-//	Serial.print(present, HEX);
-//	Serial.print(" ");
-	for ( i = 0; i < 9; i++)
-	{
-		// we need 9 bytes
-		data[i] = ds.read();
-//		Serial.print(data[i], HEX);
-//		Serial.print(" ");
-	}
-//	Serial.print(" CRC=");
-//	Serial.print(OneWire::crc8(data, 8), HEX);
-//	Serial.println();
-
-	// Convert the data to actual temperature
-	// because the result is a 16 bit signed integer, it should
-	// be stored to an "int16_t" type, which is always 16 bits
-	// even when compiled on a 32 bit processor.
-	int16_t raw = (data[1] << 8) | data[0];
-	if (type_s)
-	{
-		raw = raw << 3; // 9 bit resolution default
-		if (data[7] == 0x10)
-		{
-		  // "count remain" gives full 12 bit resolution
-		  raw = (raw & 0xFFF0) + 12 - data[6];
-		}
-	} else {
-		byte cfg = (data[4] & 0x60);
-		// at lower res, the low bits are undefined, so let's zero them
-		if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-		else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-		else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-		//// default is 12 bit resolution, 750 ms conversion time
-	}
-
-	celsius = (float)raw / 16.0;
-	fahrenheit = celsius * 1.8 + 32.0;
-	debugf("  Temperature = %f Celsius ", celsius);
-	debugf("mem %d",system_get_free_heap_size());
-//	Serial.print("  Temperature = ");
-//	Serial.print(celsius);
-//	Serial.print(" Celsius, ");
-//	Serial.println(" Fahrenheit");
-//	Serial.println();
-
-	currentTemp = celsius;
-
-	//TODO:ilan
-	if(currentDisplayMode == Display_Info) {
-		infos->updateParamValue("temp", String(currentTemp, 2), display);
-//		InfoScreenPage* iScreen = infos->get(currentInfoScreenIndex);
-//		iScreen->updateParam("temp", String(currentTemp, 3), display);
-		display.display();
-	}
-
-	checkTempTriggerRelay(celsius);
-
-//	unsigned long end = millis();
-//	debugf("Temp took %lu",  (long)(end- start));
+	oneTempReadTimer.initializeMs(1000, readAfterWait).startOnce();
 }
 
 void init()
 {
-	Serial.begin(SERIAL_BAUD_RATE); // 74880
+	Serial.begin(SERIAL_BAUD_RATE); // 115200
 	Serial.systemDebugOutput(true); // Debug output to serial
 	Serial.printf("System Start Reason : %d\r\n", system_get_rst_info()->reason);
 	Wire.pins(sclPin, sdaPin);
 
 	debugf("======= SousVide ==========");
-
-
-//	delay(3000);
-//	say("======= SousVide ==========");
 	Serial.println();
 
 	display.begin(SSD1306_SWITCHCAPVCC);
@@ -1128,10 +1140,6 @@ void init()
 
 	display.setTextSize(1);
 	display.setTextColor(WHITE);
-//	display.print("ilan1\n");
-//	display.print("ilan23\n");
-//
-//	display.display();
 
 	initMenu();
 	initInfoScreens();
@@ -1143,8 +1151,6 @@ void init()
 
 	pinMode(relayPin, OUTPUT);
 	digitalWrite(relayPin, LOW);
-//	blinkTimer.initializeMs(1000, blink).start();
-//
 	attachInterrupt(encoderA, updateEncoder, CHANGE);
 	attachInterrupt(encoderB, updateEncoder, CHANGE);
 
@@ -1155,21 +1161,18 @@ void init()
 	lastActionTime = date.Milliseconds;
 	sayln(lastActionTime);
 
-//	procTimer.initializeMs(15, handleEncoderInterrupt).start();
 	buttonTimer.initializeMs(150, checkRotaryBtn).start();
 
 	showInfoScreen();
-//	refreshScreen();
 
 	//ilan
-//	displayTimer.initializeMs(15, handleScreen).start();
 	timeTimer.initializeMs(1000, updateTimeTimerAction).start();
 
 	AppSettings.load();
 
 //	//ilan
 	ds.begin(); // It's required for one-wire initialization!
-	readTempTimer.initializeMs(3000, readTempData).start();
+	readTempTimer.initializeMs(1000, readTempData).startOnce();
 
 	WifiStation.enable(true);
 //	if (AppSettings.exist())
