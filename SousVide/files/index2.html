ee5bb513090bb34e2a28d4d145095612f0f12607
alon24 <htpcfreak@gmail.com>
2015-09-19 11:18:37 +0300

    changes to html

---
+++ SousVide/files/index2.html
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+
+<div data-role="page" id="pageone">
+  <div data-role="header">
+    <h1>Welcome To My Homepage</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>Welcome! If you click on the link below, it will take you to Page Two.</p>
+    <a href="#pagetwo">Go to Page Two</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Footer Text</h1>
+  </div>
+</div> 
+
+<div data-role="page" id="pagetwo">
+  <div data-role="header">
+    <h1>Welcome To My Homepage</h1>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <p>This is Page Two. If you click on the link below, it will take you to Page One.</p>
+    <a href="#pageone">Go to Page One</a>
+  </div>
+
+  <div data-role="footer">
+    <h1>Footer Text</h1>
+  </div>
+</div> 
+
+</body>
+</html>
---
+++ SousVide/files/main.js
@@ -0,0 +1,167 @@
+var output;
+
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function(str) {
+    return this.indexOf(str) === 0;
+  };
+}
+
+
+function init() {
+  output = document.getElementById("output");
+  // updateTime();
+  $("#flip_sous_state").attr("disabled", true);
+  $("#relay1_state").attr("disabled", true);
+
+  $("#relay1").attr("disabled", true);
+  $(document).ready(function(){
+
+    $("#flip_sous_state").attr("disabled", false);
+    $("#relay1_state").attr("disabled", false);
+
+    $("#flip_sous_state").val("leave").flipswitch('refresh');
+    $("#relay1_state").val("leave").flipswitch('refresh');
+
+    $("#flip_sous_state").change(
+      function(){
+//  	        $(this).toggleClass("down");
+        // $(this).toggleClass('off');
+        console.log("flip_sous_state clicked " + this.value);
+      });
+
+    $("#relay1_state").change(
+      function(){
+        doSend('toggleRelay:' + (this.value === 'arrive' ? 'true' : 'false'));
+        console.log("relay1_state clicked " + this.value);
+      });
+
+
+      //put in slider labels
+      //new an attempt to label the min max values on the loan Amount slider page1
+      //code found on jquery forum: https://forum.jquery.com/topic/how-do-i-add-text-labels-below-slider
+      //Note: Use of the theme for the slider induced some issues, so
+      //had to put in following inline alteration to the text are would show up with a text-shadow:
+      //text-shadow:none; color:black; font-weight:normal
+      $.fn.extend({
+        sliderLabels: function(left,right) {
+          var $this = $(this);
+          var $sliderdiv= $this.next("div.ui-slider[role='application']");
+          //
+          $sliderdiv
+          .prepend('<span class="ui-slider-inner-label" style="position: absolute; left:0px; top:20px; text-shadow:none; color:black; font-weight:normal">'+left+ '</span>')
+          // .append('<span class="ui-slider-inner-label" style="position: absolute; right:0px; bottom:20px; text-shadow:none; color:black; font-weight:normal">'+right+ '</span>');
+          //
+        }
+       });
+       //
+      $('#slider-PrincipleAmnt').sliderLabels('Min: $20', 'Max: $999');
+
+  testWebSocket();
+  });
+}
+
+var sliderChange = function(sliderValue) {
+  console.log("Slider changed: " + sliderValue);
+};
+
+function sendValueChanged(id, value) {
+  console.log(id + "="  + value);
+}
+
+function testWebSocket() {
+  try {
+    var wsUri = "ws://" + location.host + "/";
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) {
+      onOpen(evt)
+    };
+    websocket.onclose = function(evt) {
+      onClose(evt)
+    };
+    websocket.onmessage = function(evt) {
+      onMessage(evt)
+    };
+    websocket.onerror = function(evt) {
+      onError(evt)
+    };
+  }
+  catch(err) {
+      console.log("Web socket not connected");
+  }
+}
+
+function onOpen(evt) {
+  writeToScreen("CONNECTED");
+  doSend("Sming love WebSockets");
+}
+
+function onClose(evt) {
+  writeToScreen("DISCONNECTED");
+}
+
+function onMessage(evt) {
+  //    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+  //websocket.close();
+  handlePayload(evt.data);
+}
+
+
+function handlePayload(payload) {
+	//check if need to change the realy button state
+	if (payload.startsWith('relayState')) {
+		var state = payload.substring('relayState'.length + 1);
+		if (state === 'true') {
+      $("#relay1_state").val("leave").flipswitch('refresh');
+			// $('#relay1').prop('checked', (state === ));
+			// $('#relay1').addClass("ss");
+		}
+	}
+	else if (payload.startsWith('updatetime')) {
+		var newTime = payload.substring('updatetime'.length + 1);
+		updateTime(newTime);
+	}
+	else if (payload.startsWith('temp')) {
+		var newTemp = payload.substring('temp'.length + 1);
+		updateTemp(newTemp);
+	}
+}
+
+function updateTime(newTime) {
+    // newTime = "11:22:33";
+    var timeParts = newTime.split(":");
+    $('#hour').text(timeParts[0]);
+    $('#min').text(timeParts[1]);
+    $('#sec').text(timeParts[2]);
+    console.log(newTime);
+}
+
+function updateTemp(newTemp) {
+    $('#temperature').text(newTemp);
+    $('#svid_current_temp').text(newTemp);
+    console.log("temp=" + newTemp);
+}
+
+function onError(evt) {
+  // writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+}
+
+function doSend(message) {
+//  writeToScreen("SENT: " + message);
+  websocket.send(message);
+}
+
+function writeToScreen(message) {
+//  var pre = document.createElement("p");
+//  pre.style.wordWrap = "break-word";
+//  pre.innerHTML = message;
+//  output.appendChild(pre);
+}
+
+function doDisconnect() {
+  var disconnect = document.getElementById("disconnect");
+  disconnect.disabled = true;
+  websocket.close();
+}
+
+window.addEventListener("load", init, false);
